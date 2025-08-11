37835c5e2fda56b00beff93042cdcde50a5e776d
devvictor <victorritossantos@gmail.com>
2025-08-11 13:58:56 -0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-st.sidebar.header("Seu Estoque")
-janela = st.sidebar.radio("",["Inicio"])
+
+coluns = ["Material","Texto breve material","Nº de série"]
 
 arquivo = st.file_uploader("Carregue a planilha do sap",type=["ods","xlsx"])
 
 st.header("Controle de Estoque")
 
 if arquivo is not None:
-    leitor = pd.read_excel(arquivo)
+    filtro = st.text_input("Digite o material ou N° de série que deseja encontrar")
+    leitor = pd.read_excel(arquivo,usecols=coluns)
+    
+    #aplica a condição de filtro digitada pelo usuario e aplica na planilha 
+    if filtro:
+        newleitor = leitor[leitor["Nº de série"].astype(str).str.contains(filtro, case=False, na=False)]
+      
+    else:
+        newleitor = leitor
 
-    st.dataframe(leitor)
-
-
+    st.dataframe(newleitor)
